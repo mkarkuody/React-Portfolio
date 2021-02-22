27600211bde24daf547ee40e4e7a2bf6fe2c761c
mkarkuody <mkarkuody@yahoo.com>
2021-02-22 15:28:31 -0800

    added my resume link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ function Navbar() {
             <a className="nav-link" href="/Portfolio">Portfolio</a>
           </li>
           <li className="nav-item active">
-            <a className="nav-link" src="https://drive.google.com/file/d/1uGXP9Px5hNPnu1nn_RVCyeRH-iDMPkV3/view">My Resume</a>
+            <a className="nav-link" href="https://drive.google.com/file/d/1uGXP9Px5hNPnu1nn_RVCyeRH-iDMPkV3/view">My Resume</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="/">About</a>
